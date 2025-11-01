72b106aecc73d9963bcc57aeaf282d916bf44284
BAESUBEEN <bsbi1219@naver.com>
2025-11-02 04:42:15 +0900

    main 기본 틀 생성

---
+++ main.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+open_canvas()
+while running:
+    handle_events()
+    delay(0.01)
+close_canvas()
