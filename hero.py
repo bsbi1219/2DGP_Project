732aa4a1de37478a3ec2face8cb91f6a7fd3309a
BAESUBEEN <bsbi1219@naver.com>
2025-11-03 11:35:07 +0900

    hero 이미지 불러올 때 절대경로 지정

--- hero.py
+++ hero.py
@@ -1,10 +1,11 @@
 from pico2d import *
+import game_world
 
 class Hero:
     def __init__(self):
         self.x = 400
         self.y = 300
-        self.image = load_image('hero.png')
+        self.image = load_image('Assets/hero/hero_idle.png')
 
     def draw(self):
         self.image.draw(self.x, self.y)
