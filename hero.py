25ad4a6ec99616a5be95c0a91566ef07eebb3ade
BAESUBEEN <bsbi1219@naver.com>
2025-11-07 02:13:57 +0900

    hero 상태 정의

--- hero.py
+++ hero.py
@@ -1,11 +1,27 @@
 from pico2d import *
 import game_world
+from state_machine import StateMachine
+
+class Idle:
+    def __init__(self, hero):
+        self.hero = hero
 
 class Hero:
     def __init__(self):
         self.x = 400
         self.y = 300
+        self.face_dir = 1
         self.image = load_image('Assets/hero/hero_idle.png')
+        self.IDLE = Idle(self)
+
+        self.state_machine = StateMachine(self.IDLE,{ })
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.handle_state_event(('INPUT', event))
+        pass
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.state_machine.draw()
