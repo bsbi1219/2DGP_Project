027d8b5dcacb6bf00ffe8b0c65352176a2d05d98
BAESUBEEN <bsbi1219@naver.com>
2025-11-07 02:32:42 +0900

    framework에 따른 hero 속도 조절, idle함수 생성 및 정의

--- hero.py
+++ hero.py
@@ -1,5 +1,5 @@
 from pico2d import *
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
 import game_world
 import game_framework
 from state_machine import StateMachine
@@ -19,9 +19,57 @@ def left_down(e):
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+
+def up_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+
+
+def up_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+
+
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Idle:
     def __init__(self, hero):
         self.hero = hero
+
+    def enter(self, e):
+        self.hero.wait_time = get_time()
+        self.hero.dir = 0
+
+    def exit(self, e):
+            pass
+
+    def do(self):
+        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+
+    def draw(self):
+        if self.hero.face_dir == 1: # right
+            self.hero.image.clip_draw(int(self.hero.frame) * 64, 300, 64, 64, self.hero.x, self.hero.y)
+        elif self.hero.face_dir == 2: # left
+            self.hero.image.clip_draw(int(self.hero.frame) * 64, 200, 64, 64, self.hero.x, self.hero.y)
+        elif self.hero.face_dir == 3: # up
+            self.hero.image.clip_draw(int(self.hero.frame) * 64, 200, 64, 64, self.hero.x, self.hero.y)
+        elif self.hero.face_dir == 4: # down
+            self.hero.image.clip_draw(int(self.hero.frame) * 64, 200, 64, 64, self.hero.x, self.hero.y)
+
 
 class Hero:
     def __init__(self):
