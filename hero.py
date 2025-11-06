e458e2c3b921be39408eac48bfc158cdaf51c318
BAESUBEEN <bsbi1219@naver.com>
2025-11-07 02:24:28 +0900

    boy함수 import수정, 키 입력 추가

--- hero.py
+++ hero.py
@@ -1,6 +1,23 @@
 from pico2d import *
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 import game_world
+import game_framework
 from state_machine import StateMachine
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 class Idle:
     def __init__(self, hero):
