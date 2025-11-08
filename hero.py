0f1ca27435fb345ec88141eca606160d86bf91cf
BAESUBEEN <bsbi1219@naver.com>
2025-11-09 01:03:07 +0900

    .

--- hero.py
+++ hero.py
@@ -31,6 +31,7 @@ def down_up(e):
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
+
 PIXEL_PER_METER = (10.0 / 0.2)
 RUN_SPEED_KMPH = 20.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
