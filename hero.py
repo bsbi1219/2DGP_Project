0a305ec17854b386a15621230670b5dd403d6eb1
BAESUBEEN <bsbi1219@naver.com>
2025-11-08 23:34:09 +0900

    hero의 좌우상하 dir 변수 분리

--- hero.py
+++ hero.py
@@ -53,33 +53,32 @@ class Move:
 
     def enter(self, e):
         if right_down(e):
-            self.hero.dir = self.hero.face_dir = 1
+            self.hero.vx = 1
+            self.hero.vy = 0
+            self.hero.face_dir = 1
         elif left_down(e):
-            self.hero.dir = self.hero.face_dir = 2
+            self.hero.vx = -1
+            self.hero.vy = 0
+            self.hero.face_dir = 2
         elif up_down(e):
-            self.hero.dir = self.hero.face_dir = 3
+            self.hero.vx = 0
+            self.hero.vy = 1
+            self.hero.face_dir = 3
         elif down_down(e):
-            self.hero.dir = self.hero.face_dir = 4
+            self.hero.vx = 0
+            self.hero.vy = -1
+            self.hero.face_dir = 4
 
     def exit(self, e):
         pass
 
     def do(self):
         self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
-        if self.hero.dir == 1 or self.hero.dir == 2:
-            self.hero.x += self.hero.dir * RUN_SPEED_PPS * game_framework.frame_time
-        elif self.hero.dir == 3 or self.hero.dir == 4:
-            self.hero.y += (self.hero.dir - 2) * RUN_SPEED_PPS * game_framework.frame_time
+        self.hero.x += self.hero.vx * RUN_SPEED_PPS * game_framework.frame_time
+        self.hero.y += self.hero.vy * RUN_SPEED_PPS * game_framework.frame_time
 
     def draw(self):
-        if self.hero.face_dir == 1: # right
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 2: # left
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 3: # up
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 4: # down
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
+        self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
 
 class Idle:
     def __init__(self, hero):
@@ -88,7 +87,8 @@ class Idle:
 
     def enter(self, e):
         self.hero.wait_time = get_time()
-        self.hero.dir = 0
+        self.hero.vx = 0
+        self.hero.vy = 0
 
     def exit(self, e):
             pass
@@ -97,14 +97,7 @@ class Idle:
         self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
 
     def draw(self):
-        if self.hero.face_dir == 1: # right
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 2: # left
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 3: # up
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-        elif self.hero.face_dir == 4: # down
-            self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
+        self.hero.image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
 
 
 class Hero:
@@ -113,13 +106,16 @@ class Hero:
         self.y = 300
         self.frame = 0
         self.face_dir = 1
-        self.dir = 0
+        self.vx = 0
+        self.vy = 0
         self.image = load_image('Assets/hero/hero_idle.png')
 
         self.IDLE = Idle(self)
+        self.MOVE = Move(self)
 
         self.state_machine = StateMachine(self.IDLE,{
-            self.IDLE: { right_down: Move(self), left_down: Move(self), up_down: Move(self), down_down: Move(self) }
+            self.IDLE: { right_down: self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE },
+            self.MOVE: { right_up: self.IDLE, left_up: self.IDLE, up_up: self.IDLE, down_up: self.IDLE }
         })
 
     def update(self):
