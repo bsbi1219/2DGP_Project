9e495dd69b77e0642bd960a5f2bdf5655cbce73f
BAESUBEEN <bsbi1219@naver.com>
2025-11-09 02:16:41 +0900

    map에 바닥 임시 배치

--- map.py
+++ map.py
@@ -15,25 +15,28 @@ class Map:
         self.fire_w = 44
         self.fire_h = 48
 
+        width = 76
+        height = 56
+
+        self.map_1_data = []
+
         # 1은 바닥, 2는 벽
-        self.map_1_data = [
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-            [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-            [2, 1, 0, 0, 0, 0, 0, 0, 1, 2],
-            [2, 1, 0, 0, 0, 0, 0, 0, 1, 2],
-            [2, 1, 1, 1, 1, 1, 1, 1, 1, 2],
-            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-        ]
+        for y in range(height):
+            row = []
+            for x in range(width):
+                if x == 0 or x == width - 1 or y == 0 or y == height - 1:
+                    row.append(2)  # 벽
+                else:
+                    row.append(1)  # 바닥
+            self.map_1_data.append(row)
 
     def draw(self):
         for y in range(len(self.map_1_data)):
             for x in range(len(self.map_1_data[y])):
                 tile = self.map_1_data[y][x]
                 if tile == 1:
-                    self.floor_image.clip_draw(0, 0 + 16 * 8, self.floor_w, self.floor_h,
-                                               x * self.floor_w + self.floor_h / 2,
-                                               (len(self.map_1_data) - y - 1) * self.floor_w + self.floor_h / 2)
-                elif tile == 2:
-                    self.floor_image.clip_draw(16, 0, self.tile_size, self.tile_size,
-                                               x * self.tile_size + self.tile_size / 2,
-                                               (len(self.map_data) - y - 1) * self.tile_size + self.tile_size / 2)
+                    self.map_image.clip_draw(0 + 16 * 1, 0 + 16 * 16, self.map_tile_size, self.map_tile_size, x * self.map_tile_size + self.map_tile_size / 2,
+                                               (len(self.map_1_data) - y - 1) * self.map_tile_size + self.map_tile_size / 2, 16, 16)
+
+    def update(self):
+        pass
