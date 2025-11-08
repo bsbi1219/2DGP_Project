9b6d70127982bc3c06995119194acd070111ae61
BAESUBEEN <bsbi1219@naver.com>
2025-11-09 00:16:26 +0900

    a키 입력 시 공격. 공격 시 캐릭터 멈추는 문제 발생

--- hero.py
+++ hero.py
@@ -1,5 +1,5 @@
+# python
 from pico2d import *
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
 import game_world
 import game_framework
 from state_machine import StateMachine
@@ -7,30 +7,23 @@ from state_machine import StateMachine
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
 
-
 def right_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
-
 
 def left_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
 
-
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
-
 
 def up_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
 
-
 def up_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
 
-
 def down_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
-
 
 def down_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
@@ -38,9 +31,8 @@ def down_up(e):
 def a_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
-
-PIXEL_PER_METER = (10.0 / 0.2)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0  # Km / Hour
+PIXEL_PER_METER = (10.0 / 0.2)
+RUN_SPEED_KMPH = 20.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -53,19 +45,38 @@ class Attack:
     def __init__(self, hero):
         self.hero = hero
         self.height = 64 * 3
+        self.duration = 0.5
+        self.elapsed = 0.0
 
     def enter(self, e):
-        pass
+        self.hero.frame = 0
+        self.elapsed = 0.0
 
     def exit(self, e):
         pass
 
     def do(self):
-        pass
+        ft = game_framework.frame_time
+        self.elapsed += ft
+        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * ft) % 6
+
+        if self.elapsed >= self.duration:
+            self.exit(None)
+            moving = (self.hero.vx != 0 or self.hero.vy != 0)
+            next_state = self.hero.MOVE if moving else self.hero.IDLE
+            next_state.enter(('AUTO', None))
+            self.hero.state_machine.cur_state = next_state
 
     def draw(self):
-        pass
-
+        if self.hero.face_dir == 1:
+            self.height = 64 * 1
+        elif self.hero.face_dir == 2:
+            self.height = 64 * 2
+        elif self.hero.face_dir == 3:
+            self.height = 0
+        elif self.hero.face_dir == 4:
+            self.height = 64 * 3
+        self.hero.walk_attack_image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
 
 class Move:
     def __init__(self, hero):
@@ -73,30 +84,17 @@ class Move:
         self.height = 64 * 3
 
     def enter(self, e):
-        if right_down(e):
-            self.hero.vx = 1
-            self.hero.vy = 0
-            self.hero.face_dir = 1
-        elif left_down(e):
-            self.hero.vx = -1
-            self.hero.vy = 0
-            self.hero.face_dir = 2
-        elif up_down(e):
-            self.hero.vx = 0
-            self.hero.vy = 1
-            self.hero.face_dir = 3
-        elif down_down(e):
-            self.hero.vx = 0
-            self.hero.vy = -1
-            self.hero.face_dir = 4
+        # 키 홀드 방식이면 속도는 Hero.update에서 결정하므로 별도 처리 없음
+        pass
 
     def exit(self, e):
         pass
 
     def do(self):
-        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6
-        self.hero.x += self.hero.vx * RUN_SPEED_PPS * game_framework.frame_time
-        self.hero.y += self.hero.vy * RUN_SPEED_PPS * game_framework.frame_time
+        ft = game_framework.frame_time
+        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * ft) % 6
+        self.hero.x += self.hero.vx * RUN_SPEED_PPS * ft
+        self.hero.y += self.hero.vy * RUN_SPEED_PPS * ft
 
     def draw(self):
         if self.hero.face_dir == 1:
@@ -109,7 +107,6 @@ class Move:
             self.height = 64 * 3
         self.hero.walk_image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
 
-
 class Idle:
     def __init__(self, hero):
         self.hero = hero
@@ -121,14 +118,22 @@ class Idle:
         self.hero.vy = 0
 
     def exit(self, e):
-            pass
+        pass
 
     def do(self):
-        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        ft = game_framework.frame_time
+        self.hero.frame = (self.hero.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * ft) % 4
 
     def draw(self):
+        if self.hero.face_dir == 1:
+            self.height = 64 * 1
+        elif self.hero.face_dir == 2:
+            self.height = 64 * 2
+        elif self.hero.face_dir == 3:
+            self.height = 0
+        elif self.hero.face_dir == 4:
+            self.height = 64 * 3
         self.hero.idle_image.clip_draw(int(self.hero.frame) * 64, self.height, 64, 64, self.hero.x, self.hero.y, 200, 200)
-
 
 class Hero:
     def __init__(self):
@@ -142,17 +147,18 @@ class Hero:
 
         self.idle_image = load_image('Assets/hero/hero_idle.png')
         self.walk_image = load_image('Assets/hero/hero_walk.png')
+        self.walk_attack_image = load_image('Assets/hero/hero_walk_attack.png')
 
         self.IDLE = Idle(self)
         self.MOVE = Move(self)
         self.ATTACK = Attack(self)
 
-        self.state_machine = StateMachine(self.IDLE,{
-            self.IDLE: { right_down: self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE },
+        self.state_machine = StateMachine(self.IDLE, {
+            self.IDLE: { right_down: self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE, a_down: self.ATTACK },
             self.MOVE: { right_up: self.IDLE, left_up: self.IDLE, up_up: self.IDLE, down_up: self.IDLE,
-                         right_down : self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE,
+                         right_down: self.MOVE, left_down: self.MOVE, up_down: self.MOVE, down_down: self.MOVE,
                          a_down: self.ATTACK },
-            self.ATTACK: { }
+            self.ATTACK: {}
         })
 
     def update(self):
@@ -198,15 +204,14 @@ class Hero:
         elif event.type == SDL_KEYUP:
             self.keys_pressed.discard(event.key)
 
+        self.state_machine.handle_state_event(('INPUT', event))
+
     def draw(self):
         self.state_machine.draw()
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         return self.x - 32, self.y - 32, self.x + 32, self.y + 32
-
-    def draw(self):
-        self.state_machine.draw()
 
     def handle_collision(self, group, other):
         pass
