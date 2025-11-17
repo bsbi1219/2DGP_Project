8f990285edecc0fcdae89485a7df5d6585d92650
BAESUBEEN <bsbi1219@naver.com>
2025-11-17 18:57:52 +0900

    camera 이용한 draw 및 w, h좌표 읽기

--- map_1.py
+++ map_1.py
@@ -1,4 +1,5 @@
 import csv
+import game_world
 from pico2d import *
 
 # 가로 13개, 세로 23개 타일
@@ -35,34 +36,49 @@ class Map:
         f_read('csv/맵 1번_거미 여왕 맵 오브젝트_spider.csv', self.map_1_object_spider)
         f_read('csv/맵 1번_거미 여왕 맵 오브젝트_web.csv', self.map_1_object_wep)
 
-    def draw_map(self, height, map_list, map_image):
+        if len(self.map_1_floor) == 0:
+            raise Exception("맵 바닥 CSV를 읽지 못했음")
+        self.map_width = len(self.map_1_floor[0]) * self.tile_size
+        self.map_height = len(self.map_1_floor) * self.tile_size
+
+    def draw_map(self, height, map_list, map_image, camera):
         cols = map_image.w // self.tile_size
         image_h = map_image.h
+
         for y in range(height):
             for x in range(len(map_list[y])):
-                tile = map_list[y][x]
-                idx = tile
+                idx = map_list[y][x]
                 if idx < 0:
                     continue
+
                 sx = (idx % cols) * self.tile_size
                 sy = image_h - (idx // cols + 1) * self.tile_size
-                map_image.clip_draw(sx, sy, self.tile_size, self.tile_size,
-                                    x * self.tile_size + self.tile_size / 2,
-                                    (height - y - 1) * self.tile_size + self.tile_size / 2)
+
+                wx = x * self.tile_size + self.tile_size / 2
+                wy = (height - y - 1) * self.tile_size + self.tile_size / 2
+
+                camera.draw_image(
+                    map_image,
+                    sx, sy,
+                    self.tile_size, self.tile_size,
+                    wx, wy,
+                    self.tile_size, self.tile_size
+                )
 
     def draw(self):
+        cam = game_world.camera
         height_floor = len(self.map_1_floor)
         height_wall = len(self.map_1_wall)
         height_object = len(self.map_1_objects)
         height_object_cave = len(self.map_1_object_cave)
         height_object_spider = len(self.map_1_object_spider)
         height_object_wep = len(self.map_1_object_wep)
-        self.draw_map(height_floor, self.map_1_floor, self.floor_image)
-        self.draw_map(height_wall, self.map_1_wall, self.wall_image)
-        self.draw_map(height_object, self.map_1_objects, self.object_image)
-        self.draw_map(height_object_cave, self.map_1_object_cave, self.object_cave_image)
-        self.draw_map(height_object_spider, self.map_1_object_spider, self.object_spider_image)
-        self.draw_map(height_object_wep, self.map_1_object_wep, self.object_web_image)
+        self.draw_map(height_floor, self.map_1_floor, self.floor_image, cam)
+        self.draw_map(height_wall, self.map_1_wall, self.wall_image, cam)
+        self.draw_map(height_object, self.map_1_objects, self.object_image, cam)
+        self.draw_map(height_object_cave, self.map_1_object_cave, self.object_cave_image, cam)
+        self.draw_map(height_object_spider, self.map_1_object_spider, self.object_spider_image, cam)
+        self.draw_map(height_object_wep, self.map_1_object_wep, self.object_web_image, cam)
 
 
     def update(self):
