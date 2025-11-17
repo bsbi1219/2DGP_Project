18fedd37b1ed982a1361e2a949ea95954f964bcf
BAESUBEEN <bsbi1219@naver.com>
2025-11-18 01:05:59 +0900

    ui py, class 생성

---
+++ ui.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+class StateUI:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def handle_event(self, event):
+        pass
+
+class InventoryUI:
+    def __init__(self):
+        pass
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def handle_event(self, event):
+        pass
