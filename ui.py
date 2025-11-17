950b16a8a18742315f3d62c6d391bc4898aae5bc
BAESUBEEN <bsbi1219@naver.com>
2025-11-18 01:47:58 +0900

    ui class

--- ui.py
+++ ui.py
@@ -1,21 +1,29 @@
 from pico2d import *
+import game_framework
+import game_world
 
 class StateUI:
     def __init__(self):
-        pass
+        self.image = load_image('Assets/UI/ui.png')
+
     def draw(self):
-        pass
+        self.image.draw(250, 90)
+
     def update(self):
         pass
+
     def handle_event(self, event):
         pass
 
 class InventoryUI:
     def __init__(self):
         pass
+
     def draw(self):
         pass
+
     def update(self):
         pass
+
     def handle_event(self, event):
         pass
