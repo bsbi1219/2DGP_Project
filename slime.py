2f8260bfc733537e45a6c59a8a56b8db9a279d14
BAESUBEEN <bsbi1219@naver.com>
2025-11-18 02:14:24 +0900

    slime 속도, 애니메이션 속도 변경 및 face dir에 따른 움직임

--- slime.py
+++ slime.py
@@ -4,13 +4,13 @@ import game_world
 from state_machine import StateMachine
 import random
 
-PIXEL_PER_METER = (10.0 / 1.0)
+PIXEL_PER_METER = (10.0 / 1.2)
 RUN_SPEED_KMPH = 10.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 0.5
+TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10.0
 
@@ -27,11 +27,11 @@ class Slime:
 
     def __init__(self):
         self.x, self.y = random.randint(100, 1000), random.randint(100, 900)
-        self.face_dir = 1
+        self.face_dir = 1 # 앞뒤좌우
         self.load_images()
         self.frame = random.randint(0, 7)
-        self.vx = random.choice([-1, 1])
-        self.vy = random.choice([-1, 1])
+        self.vx = 1
+        self.vy = 1
         self.wait_time = get_time()
         self.height = 64 * 3
         self.hp = 150
@@ -43,23 +43,31 @@ class Slime:
         pass
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
         if get_time() - self.wait_time > 2:
             self.wait_time = get_time()
             self.face_dir = random.randint(1, 4)
+        if self.face_dir == 1:
+            self.y -= self.vy * RUN_SPEED_PPS * game_framework.frame_time
+        elif self.face_dir == 2:
+            self.y += self.vy * RUN_SPEED_PPS * game_framework.frame_time
+        elif self.face_dir == 3:
+            self.x -= self.vx * RUN_SPEED_PPS * game_framework.frame_time
+        elif self.face_dir == 4:
+            self.x += self.vx * RUN_SPEED_PPS * game_framework.frame_time
         pass
 
     def draw(self):
         cam = game_world.camera
         sx, sy = cam.world_to_screen(self.x, self.y)
         if self.face_dir == 1:
-            self.height = 64 * 1
+            self.height = 64 * 3
         elif self.face_dir == 2:
             self.height = 64 * 2
         elif self.face_dir == 3:
-            self.height = 0
+            self.height = 64
         elif self.face_dir == 4:
-            self.height = 64 * 3
+            self.height = 0
         Slime.images[self.slime_state].clip_draw(int(self.frame) * 64, self.height, 64, 64, sx, sy, 200, 200)
 
     def handle_event(self, event):
