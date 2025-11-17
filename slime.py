e21541e3396ed03c445053627e4f1ac374c6dd9c
BAESUBEEN <bsbi1219@naver.com>
2025-11-18 00:39:33 +0900

    slime 이동 속도, 액션 속도

--- slime.py
+++ slime.py
@@ -3,6 +3,16 @@ import game_framework
 from state_machine import StateMachine
 import random
 
+PIXEL_PER_METER = (10.0 / 1.0)
+RUN_SPEED_KMPH = 10.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10.0
+
 class Slime:
     images = None
 
