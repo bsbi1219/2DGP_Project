2f7b50d2a3924dff523e32463f1949d73c930429
BAESUBEEN <bsbi1219@naver.com>
2025-11-21 23:54:02 +0900

    import 삭제

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from pico2d import open_canvas, delay, close_canvas
 import game_framework
 import title_state
 
-import play_mode as start_mode
-
 open_canvas(1280, 960)
 game_framework.run(title_state)
 close_canvas()
