bb803bb0677c1fc46d1e3999e6a7e656f669361e
BAESUBEEN <bsbi1219@naver.com>
2025-11-24 00:32:27 +0900

    충돌 처리를 위한 wall.py생성

---
+++ wall.py
@@ -0,0 +1,15 @@
+class Wall:
+    def __init__(self, left, bottom, right, top):
+        self.left = left
+        self.bottom = bottom
+        self.right = right
+        self.top = top
+
+    def get_bb(self):
+        return self.left, self.bottom, self.right, self.top
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
