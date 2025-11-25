c2edd1adedb1756ee245a43a17926252df2e7efd
BAESUBEEN <bsbi1219@naver.com>
2025-11-25 23:45:41 +0900

    hero 머리 위 체력바 추가

--- ui.py
+++ ui.py
@@ -6,16 +6,23 @@ class StateUI:
     def __init__(self):
         self.ui_image = load_image('Assets/UI/ui.png')
         self.hp_image = load_image('Assets/UI/2 Bars/HealthBar4.png')
+        self.hero_image = load_image('Assets/icon/hero.png')
 
     def draw(self):
         hero = game_world.hero
+
         self.ui_image.draw(250, 90)
+        self.hero_image.clip_draw(0, 0, 1024, 1024, 90, 90, 100, 100)
+
         hp_ratio = hero.hp / hero.max_hp
         clip_width = int(32 * hp_ratio)
         draw_width = int(310 * hp_ratio)
-
-        # 왼쪽 끝을 116으로 고정 (180 - 64 = 116)
         self.hp_image.clip_draw(0, 0, clip_width, 14, 160 + draw_width / 2, 50, draw_width, 32)
+
+        cam_hero_x, cam_hero_y = game_world.camera.world_to_screen(hero.x, hero.y)
+        clip_width = int(32 * hp_ratio)
+        draw_width = int(90 * hp_ratio)
+        self.hp_image.clip_draw(0, 0, clip_width, 14, cam_hero_x + draw_width / 2 - 50, cam_hero_y + 110, draw_width, 20)
 
     def update(self):
         pass
