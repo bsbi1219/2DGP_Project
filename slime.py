e754b40407f7c58bae9fa6c1e8c7aa7b72816751
BAESUBEEN <bsbi1219@naver.com>
2025-12-01 21:01:46 +0900

    함수명 오타 수정 및 획득 경험치 조정

--- slime.py
+++ slime.py
@@ -79,7 +79,7 @@ class Slime:
             self.slime_state = 'Walk'
         if self.slime_state == 'Death':
             if int(self.frame) >= 9:
-                game_world.hero.add_exp(5)
+                game_world.hero.get_exp(6)
                 game_world.remove_object(self)
             return
         if self.face_dir == 1:
