0ef2985a11804dd4ed961f623855f320dd6c0dbb
BAESUBEEN <bsbi1219@naver.com>
2025-12-07 02:07:34 +0900

    대사 여러개 가능하도록 수정

--- dialogue_state.py
+++ dialogue_state.py
@@ -3,23 +3,34 @@ import game_framework
 
 font = None
 ui = None
-message = ""
+
+messages = []
+msg_index = 0
+on_close = None
 
 def init():
-    global font, ui
+    global font, ui, messages, msg_index
     font = load_font("Assets/DNFBitBitv2.otf", 30)
     ui = load_image("Assets/UI/message_ui.png")
+    msg_index = 0
 
 def set_message(text):
-    global message
-    message = text
+    global messages
+    messages.append(text)
 
 def handle_events():
+    global msg_index, messages, on_close
     events = get_events()
     for e in events:
-        # 스페이스 or 엔터 누르면 대화 종료
         if e.type == SDL_KEYDOWN and (e.key == SDLK_SPACE or e.key == SDLK_RETURN):
-            game_framework.pop_mode()
+            msg_index += 1
+            if msg_index >= len(messages):
+                if on_close:
+                    on_close()
+                msg_index = 0
+                messages.clear()
+                game_framework.pop_mode()
+
 
 def update():
     pass
@@ -28,7 +39,8 @@ def draw():
     import game_world
     game_world.render()
     ui.draw(640, 480)
-    font.draw(90, 200, message, (0, 0, 0))
+    if messages:
+        font.draw(90, 200, messages[msg_index], (0, 0, 0))
     update_canvas()
 
 def pause():
